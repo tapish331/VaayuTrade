8312240051b8fa191b4434a895d2d6f4ea422b4c
tapish331 <89255633+tapish331@users.noreply.github.com>
2025-08-27 13:28:39 +0530

    ci(node): migrate to ESLint v9 flat config (eslint.config.mjs)

--- js-tests/sanity.test.js
+++ js-tests/sanity.test.js
@@ -1,6 +1,5 @@
-/* eslint-disable n/no-unsupported-features/node-builtins */
-import test from 'node:test';
 import assert from 'node:assert/strict';
+import test from 'node:test';
 
 test('sanity', () => {
   assert.equal(1 + 1, 2);
