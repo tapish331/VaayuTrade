9252e85c7eb1c81dabf532a756dbcb72b5abcc81
tapish331 <89255633+tapish331@users.noreply.github.com>
2025-08-27 11:47:12 +0530

    ci: add GitHub Actions CI skeleton (py311 + node LTS, cached)

---
+++ js-tests/sanity.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable n/no-unsupported-features/node-builtins */
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+test('sanity', () => {
+  assert.equal(1 + 1, 2);
+});
